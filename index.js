08a090da18ec43b874bf339fcfb227d3633f2b59
Kim Sanghyeok <skyee23@gmail.com>
2018-05-16 14:53:58 +0900

    npm start setting

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/static', express.static('public'))
+
+app.get('/',(req,res) =>{
+  res.render('index.ejs');
+})
+
+app.listen(3000, () => {
+  console.log('listening...')
+})
