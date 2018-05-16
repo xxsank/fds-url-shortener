1e7172948388654e4c9e69636193330a7e90214d
Kim Sanghyeok <skyee23@gmail.com>
2018-05-16 15:53:50 +0900

    example data practice

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const morgan = require('morgan')
+const randomstring = require('randomstring');
 
 const app = express()
+
+const urls = [
+  {
+    slug: randomstring.generate(8),
+    longUrl: 'https://www.naver.com'
+  }
+]
 
 app.use('/static', express.static('public'))
 app.use(morgan('dev'))
 
 app.get('/',(req,res) =>{
-  res.render('index.ejs');
+  res.render('index.ejs',{urls})
+})
+
+app.get('/:slug', (req,res) => {
+  const urlItem = urls.find(item => item.slug === req.params.slug)
+  if(urlItem){
+    res.redirect(301,urlItem.longUrl)
+  } else {
+    res.status(400)
+    res.send('404 NOT FOUND')
+  }
 })
 
 app.listen(3000, () => {
