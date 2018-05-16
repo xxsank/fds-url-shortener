bbbf5b0cbfcdfb774af6a83d4922302fba399e75
Kim Sanghyeok <skyee23@gmail.com>
2018-05-16 15:16:43 +0900

    morgan setting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use('/static', express.static('public'))
+app.use(morgan('dev'))
 
 app.get('/',(req,res) =>{
   res.render('index.ejs');
